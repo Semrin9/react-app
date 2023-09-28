a8bfeced81dcec35b733fa9600d26b1f35aef11b
semrin9 <aboodsemrin@hotmail.com>
2023-09-28 18:38:12 +0300

    Task 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import Header from "./components/header";
-import Main from "./main";
+import Main from "./main.js";
+import Products from "./products";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 function App() {
 
   return (
     <>
       <Header/>
-      
-      <Main/>
+      <Router>
+        <Routes>
+        <Route path="/" element={<Main />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,19 +1,45 @@
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 
 function CardComp(props) {
+  const [show, setShow] = useState(false);
+
+
+  function handleShow() {
+    setShow(!show);
+  }
+
   return (
-    <Card style={{ width: "16rem" }}>
-      <Card.Img variant="top" src={props.image} />
-      <Card.Body>
-        <Card.Title style={{ textAlign: "center"}}>
-          {props.title}
-        </Card.Title>
-        <Card.Text>write a description</Card.Text>
-        <Button variant="primary">View Recipe </Button>
-      </Card.Body>
-    </Card>
+    <>
+      <Card style={{ width: "16rem" }}>
+        <Card.Img variant="top" src={props.image} />
+        <Card.Body>
+          <Card.Title style={{ textAlign: "center"}}>
+            {props.title}
+          </Card.Title>
+          <Card.Text>write a description</Card.Text>
+          <Button variant="primary" onClick={handleShow}>View Recipe </Button>
+        </Card.Body>
+      </Card>
+
+      <Modal show={show} onHide={handleShow}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{props.description}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleShow}> 
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleShow}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 export default CardComp;
