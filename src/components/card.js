eaf1aee5e9c5d8042e4470041ddab00da81a1125
semrin9 <aboodsemrin@hotmail.com>
2023-10-04 02:08:33 +0300

    Task 5

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { ModalHeader } from "react-bootstrap";
@@ -18,20 +18,14 @@ function CardComp(props) {
       <Card style={{ width: "16rem", margin: "auto" }}>
         <Card.Img variant="top" src={props.image} />
         <Card.Body>
-          <Card.Title style={{ textAlign: "center"}}>
-            {props.title}
-          </Card.Title>
-          <Card.Text style={{ fontWeight: "bold", textAlign:"center",fontSize:"20px"}}>
-            Category: {props.category}
-          </Card.Text>
-          <Button variant="primary" onClick={handleShow}>
-            Show Recipe
-          </Button>
+          <Card.Title style={{ textAlign: "center"}}>{props.title}</Card.Title>
+          <Card.Text style={{ fontWeight: "bold", textAlign:"center",fontSize:"20px"}}>Category: {props.Category}</Card.Text>
+          <Button variant="primary" onClick={handleShow}>Show Recipe</Button>
         </Card.Body>
       </Card>
 
       <Modal show={show} onHide={handleShow}>
-        <Modal.Header closeButton>
+        <Modal.Header closeButton onClick={handleClose}>
           <Modal.Title>{props.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>{props.description}</Modal.Body>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,32 @@
 import CardComp from './components/card.js';
-import data from './data/data.json';
-import { useState } from 'react';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-
+import { useEffect, useState } from "react";
 
 function Main() {
-  let [items, setItems] = useState(data);
+  let [items, setItems] = useState([]);
+
+  async function getData() {
+    const url = "https://www.themealdb.com/api/json/v1/1/search.php?f=b";
+
+
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      setItems(result.meals);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect( () => {
+    getData();
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
-
-let searchedValue = event.target.search.value;
-let filteredValue = data.filter(function (item) {
-      return item.title.toLowerCase().includes(searchedValue.toLowerCase());
-    });
-    setItems(filteredValue);
   }
+
   return (
     <>
       <Form className="d-flex" style={{padding: "0.5rem 0", maxWidth: "600px", margin: "auto"}} onSubmit={handleSubmit}>
@@ -43,9 +53,10 @@ let filteredValue = data.filter(function (item) {
         {items.map(function (item) {
           return (
             <CardComp
-              image={item.image_url}
-              title={item.title}
-              description={item.description}
+            image={item.strMealThumb}
+            title={item.strMeal}
+            description={item.strInstructions}
+            Category={item.strCategory}
             />
           );
         })}
@@ -54,3 +65,13 @@ let filteredValue = data.filter(function (item) {
   );
 }
 export default Main;
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+
+//   let searchedValue = event.target.search.value;
+//   let filteredValue = items.filter(function (item) {
+//     return item.title.toLowerCase().includes(searchedValue.toLowerCase());
+//   });
+//   setItems(filteredValue);
+// }
